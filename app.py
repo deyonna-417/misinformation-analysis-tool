b1741d07166b8afa1afc0beb71877df822ba279f
deyonna-417 <417ringo0307seina@gmail.com>
2026-06-29 14:52:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ from bs4 import BeautifulSoup
 # =========================
 
 ALLOWED_DOMAINS = [
-    "news.web.nhk"
-    "nhk",
+    "news.web.nhk",
+    "nhk.or.jp",
     "mainichi.jp",
     "asahi.com",
     "yomiuri.co.jp",
